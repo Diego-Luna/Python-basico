5e9b6668baef72817269caf1e68cb1977de55421
Dieg Luna <diegofcolunalopez@gmail.com>
2021-11-06 11:53:32 -0600

    Construyendo el camino de un programa con condicionales

---
+++ codigo_python/conceptos/condicionales.py
@@ -0,0 +1,15 @@
+
+# edad = int(input('Escribe tu eddad: '))
+
+# if edad > 17:
+#     print('Eres mayor de edad')
+# else:
+#     print('Eres menor de edad')
+
+edad = int(input('Escribe tu eddad: '))
+if numero > 5:
+    print('Es mayor a 5')
+elif numero == 5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5')
